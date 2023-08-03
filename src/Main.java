3db60443eb60d074ea6a23a71861bebd55ded442
dem0N4k <demON4k1993@gmail.com>
2023-08-04 02:46:18 +0300

    add Lesson 3

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+//        System.out.println("B " + 4 + " gb: " + (4 * 1024) + " mb");
+
+        // масса (кг)/ рост2 (м2)
+        System.out.println("\t\t\t Привет \t\t\t");
+        System.out.println("BMI: " + 75.0 / (180 * 180));
+
+    }
+}
